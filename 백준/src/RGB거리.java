240c1b17ed50b786760fcc34459d8049f3e37d5b
junbeom <jach1206@naver.com>
2025-06-02 16:52:05 +0900

    solved: backjoon_1149 - RGB 거리

---
+++ 백준/src/RGB거리.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class RGB거리 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        int[][] arr = new int[n + 1][3];
+        int[][] dp = new int[n + 1][3];
+
+        for (int i = 1; i <= n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            arr[i][0] = Integer.parseInt(st.nextToken());
+            arr[i][1] = Integer.parseInt(st.nextToken());
+            arr[i][2] = Integer.parseInt(st.nextToken());
+        }
+
+        dp[1][0] = arr[1][0];
+        dp[1][1] = arr[1][1];
+        dp[1][2] = arr[1][2];
+
+        for (int i = 2; i <= n; i++) {
+            dp[i][0] = Math.min(dp[i - 1][1], dp[i - 1][2]) + arr[i][0];
+            dp[i][1] = Math.min(dp[i - 1][0], dp[i - 1][2]) + arr[i][1];
+            dp[i][2] = Math.min(dp[i - 1][0], dp[i - 1][1]) + arr[i][2];
+        }
+
+        int answer = Math.min(dp[n][0], Math.min(dp[n][1], dp[n][2]));
+        System.out.println(answer);
+    }
+}
